148c732766afe7de541d4f796cf451bab6dd2ba2
JEduardo <eduardomagalhaesgmr84@gmail.com>
2019-04-04 14:32:00 +0100

    Add purchase request

---
+++ src/Message/AbstractRequest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Omnipay\AirTM\Message;
+
+use Omnipay\Common\Exception\InvalidResponseException;
+use Omnipay\Common\Message\AbstractRequest as BaseAbstractRequest;
+
+/**
+ * Abstract request.
+ */
+
+class AbstractRequest extends BaseAbstractRequest
+{
+  /**
+   * Live endpoint.
+   */
+
+  protected $liveEndpoint = 'https://purchases.airtm.io';
+
+  /**
+   * Test endpoint.
+   */
+
+  protected $testEndpoint = 'https://purchases.staging.airtm.org';
+
+  /**
+   * Get data.
+   */
+
+  public function getData()
+  {
+    return [
+      'amount' => $this->getAmount(),
+      'description' => $this->getDescription()
+    ];
+  }
+
+  /**
+   * Get partner id.
+   */
+
+  public function getPartnerId()
+  {
+    return $this->getParameter('partnerId');
+  }
+
+  /**
+   * Set partner id.
+   */
+
+  public function setPartnerId(string $partnerId)
+  {
+    return $this->setParameter('partnerId', $partnerId);
+  }
+
+  /**
+   * Get partner secret.
+   */
+
+  public function getPartnerSecret()
+  {
+    return $this->getParameter('partnerSecret');
+  }
+
+  /**
+   * Set partner secret.
+   */
+
+  public function setPartnerSecret(string $partnerSecret)
+  {
+    return $this->setParameter('partnerSecret', $partnerSecret);
+  }
+
+  /**
+   * Get authorization.
+   */
+
+  public function getAuthorization()
+  {
+    $partnerId = $this->getPartnerId();
+    $partnerSecret = $this->getPartnerSecret();
+
+    return base64_encode("$partnerId:$partnerSecret");
+  }
+
+  /**
+   * Send data.
+   */
+
+  public function sendData($data)
+  {
+    try {
+      $authorizationToken = $this->getAuthorization();
+      $headers = [
+        'Content-Type' => 'application/json',
+        'Authorization' => "Basic $authorizationToken"
+      ];
+
+      $body = $this->getHttpMethod() === 'GET' ? null : json_encode($data);
+      $httpResponse = $this->httpClient->request(
+        $this->getHttpMethod(),
+        $this->getEndpoint(),
+        $headers,
+        $body
+      );
+
+      $body = (string) $httpResponse->getBody()->getContents();
+      $jsonToArrayResponse = !empty($body) ? json_decode($body, true) : array();
+
+      return $this->createResponse($jsonToArrayResponse, $httpResponse->getStatusCode());
+    } catch(\Exception $e) {
+      throw new InvalidResponseException(
+        'Error communicating with payment gateway: ' . $e->getMessage(),
+        $e->getCode()
+      );
+    }
+  }
+
+  /**
+   * Create response.
+   */
+
+  protected function createResponse($data)
+  {
+    return $this->response = new Response($this, $data);
+  }
+
+}
